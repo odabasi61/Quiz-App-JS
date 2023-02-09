8d5acc9147c40c5d1148a1587b8bde4b58e79528
odabasi61 <aliodabas_a@hotmail.com>
2023-02-10 00:11:17 +0300

    question list prepared

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ const exitBtn = infoBox.querySelector(".buttons .quit");
 const continueBtn = infoBox.querySelector(".buttons .restart");
 const quizBox = document.querySelector(".quiz_box");
 
+const optionList = document.querySelector('.option_list');
+
+let questionCount = 0;
+let questionNumber = 1;
+
 // start button click
 startBtn.onclick = () => {
   infoBox.classList.add("activeInfo"); // shows the info box
@@ -19,4 +24,62 @@ exitBtn.onclick = () => {
 continueBtn.onclick = () => {
   infoBox.classList.remove("activeInfo"); // hides the info box
   quizBox.classList.add("activeQuiz"); // begin the quiz
+  showQuestions(0);
+  questionCounter(1);
 };
+
+// next button
+const nextBtn = quizBox.querySelector('.next_btn');
+nextBtn.onclick = () => {
+  if (questionCount < questions.length - 1) {
+    questionCount++;
+    questionNumber++;
+    showQuestions(questionCount);
+    questionCounter(questionNumber);
+  }
+  else {
+    console.log('questions completed');
+  }
+}
+
+// getting questions and answers from the array
+function showQuestions(index) {
+  const questionText = document.querySelector('.que_text');
+  let questionTag = '<span>' + questions[index].numb + '. ' + questions[index].question + '</span>';
+  let optionTag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>' + '<div class="option">'+ questions[index].options[1] +'<span></span></div>' + '<div class="option">'+ questions[index].options[2] +'<span></span></div>' + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
+  questionText.innerHTML = questionTag;
+  optionList.innerHTML = optionTag;
+  const option = optionList.querySelectorAll('.option');
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute('onclick', 'optionSelected(this)');
+    
+  }
+}
+
+// option selecting
+function optionSelected(answer) {
+  let userAnswer = answer.textContent;
+  let correctAnswer = questions[questionCount].answer;
+  let allOptions = optionList.children.length;
+  if (userAnswer == correctAnswer) {
+    answer.classList.add('correct');
+  }
+  else {
+    answer.classList.add('incorrect');
+  }
+
+  // once user made a choice, disable other options
+  for (let i = 0; i < allOptions; i++) {
+    optionList.children[i].classList.add('disabled');    
+
+    // if answer is incorrect, automatically show the correct answer
+    
+  }
+}
+
+// count the questions
+function questionCounter(index) {
+const bottomQuestionCounter = quizBox.querySelector('.total_que');
+let totalQuestionTag = '<span><p>' + index + '</p>of<p>' + questions.length + '</p>Questions</span>';
+bottomQuestionCounter.innerHTML = totalQuestionTag;
+}
